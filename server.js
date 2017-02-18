901bb487cbbd33dc029f4ecf8e9dc1bd91ee7cf2
jananisairam <januakshitha97@gmail.com>
2017-02-18 19:55:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/Personal-details',function(req,res)
+app.get('/Personal_details',function(req,res)
 {
    res.sendFile(path.join(__dirname, 'ui', 'Personal_details.html')); 
 });
-app.get('/Academic-details',function(req,res)
+app.get('/Academic_details',function(req,res)
 {
     res.send("Academic details are served here!");
 });
-app.get('/Other-details',function(req,res)
+app.get('/Other_details',function(req,res)
 {
     res.send("Other details are served here!");
 });
